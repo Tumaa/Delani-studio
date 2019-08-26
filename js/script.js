0f1a10f1a6d91d6d4b2dd77df1d52d7a7034a71e
tumaa <ramanfatuu@gmail.com>
2019-08-26 17:32:34 +0300

    created sectipn one with an image and h1 element

--- js/script.js
+++ js/script.js
@@ -1,45 +1,45 @@
-$(document).ready(function () {
-  $("#clickable1").click(function () {
-    $(".hide-1").slideToggle(1200);
-    $(".show-1").slideToggle(1200);
-  });
+// $(document).ready(function () {
+//   $("#clickable1").click(function () {
+//     $(".hide-1").slideToggle(1200);
+//     $(".show-1").slideToggle(1200);
+//   });
   
-  $("#clickable2").click(function () {
-    $(".hide-2").slideToggle(1200);
-    $(".show-2").slideToggle(1200);
-  });
+//   $("#clickable2").click(function () {
+//     $(".hide-2").slideToggle(1200);
+//     $(".show-2").slideToggle(1200);
+//   });
   
-  $("#clickable3").click(function () {
-    $(".hide-3").slideToggle(1200);
-    $(".show-3").slideToggle(1200);
-  });
+//   $("#clickable3").click(function () {
+//     $(".hide-3").slideToggle(1200);
+//     $(".show-3").slideToggle(1200);
+//   });
  
 
  
 
-$(".card").hover(function () {
-  $(this).children(".card-1").fadeToggle('fast', "linear");
-});
- });
+// $(".card").hover(function () {
+//   $(this).children(".card-1").fadeToggle('fast', "linear");
+// });
+//  });
 
 
 
 
-$('.card').hover(function(event){
-  $("form#myForm").submit(function (isiolo) {
-    var client = $('#name').val();
-    alert(' Dear  esteemed ' + client + ' we have received your message successful. Feel free to reach out to us anytime');
-    isiolo.preventDefault();
-  });
+// $('.card').hover(function(event){
+//   $("form#myForm").submit(function (isiolo) {
+//     var client = $('#name').val();
+//     alert(' Dear  esteemed ' + client + ' we have received your message successful. Feel free to reach out to us anytime');
+//     isiolo.preventDefault();
+//   });
 
 
-  //    reset my form
-  $("button").on('click', function () {
-    $Lorem('form').each(function () {
-      this.reset();
-    });
-  });
-  });
+//   //    reset my form
+//   $("button").on('click', function () {
+//     $Lorem('form').each(function () {
+//       this.reset();
+//     });
+//   });
+//   });
 
 
 
